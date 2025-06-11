d3ffe28e043c3f3d57b9db980798dc36d4e31611
Dimka122 <kassindima1@gmail.com>
2025-06-11 09:55:29 +0300

    Servises

---
+++ PersonApi/Services/IPersonService.cs
@@ -0,0 +1,10 @@
+﻿using PersonApi.DTOs;
+
+namespace PersonApi.Services
+{
+    public interface IPersonService
+    {
+        Task<PersonDTO> CreatePerson(CreatePersonDTO createPersonDto);
+        Task<IEnumerable<PersonDTO>> GetFilteredPersons(GetAllRequest request);
+    }
+}
---
+++ PersonApi/Services/PersonService.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using PersonApi.DTOs;
+using PersonApi.Models;
+using PersonApi.Repositories;
+
+namespace PersonApi.Services
+{
+    public class PersonService : IPersonService
+    {
+        private readonly IPersonRepository _personRepository;
+        private readonly IMapper _mapper;
+
+        public PersonService(IPersonRepository personRepository, IMapper mapper)
+        {
+            _personRepository = personRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<PersonDTO> CreatePerson(CreatePersonDTO createPersonDto)
+        {
+            var person = _mapper.Map<Person>(createPersonDto);
+            var createdPerson = await _personRepository.AddPerson(person);
+            return _mapper.Map<PersonDTO>(createdPerson);
+        }
+
+        public async Task<IEnumerable<PersonDTO>> GetFilteredPersons(GetAllRequest request)
+        {
+            var persons = await _personRepository.GetFilteredPersons(
+                request.FirstName,
+                request.LastName,
+                request.City);
+
+            return _mapper.Map<IEnumerable<PersonDTO>>(persons);
+        }
+    }
+}
