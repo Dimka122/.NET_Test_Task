bb7636853313911aaae037162814ec8b6e5eb8ef
Dimka122 <kassindima1@gmail.com>
2025-06-11 09:00:45 +0300

    Models

---
+++ PersonApi/Models/Address.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PersonApi.Models
+{
+    public class Address
+    {
+        public long Id { get; set; }
+        public string City { get; set; }
+        public string AddressLine { get; set; }
+
+        public virtual ICollection<Person> Persons { get; set; }
+    }
+}
---
+++ PersonApi/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace PersonApi.Models
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public long? AddressId { get; set; }
+        public virtual Address Address { get; set; }
+    }
+}
