c1b76a6917811e58c95a380055b50a95970739a9
Dimka122 <kassindima1@gmail.com>
2025-06-11 09:03:12 +0300

    DbContext

---
+++ PersonApi/Data/PersonDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonApi.Models;
+
+namespace PersonApi.Data
+{
+    public class PersonDbContext: DbContext
+    {
+        public PersonDbContext(DbContextOptions<PersonDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>()
+                .HasOne(p => p.Address)
+                .WithMany(a => a.Persons)
+                .HasForeignKey(p => p.AddressId);
+        }
+    }
+}
