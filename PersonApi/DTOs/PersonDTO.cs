6ef84d6510a83cf1a31815c3334f3964df07c196
Dimka122 <kassindima1@gmail.com>
2025-06-11 09:39:21 +0300

    DTOs

---
+++ PersonApi/DTOs/PersonDTO.cs
@@ -0,0 +1,31 @@
+﻿namespace PersonApi.DTOs
+{
+    public class PersonDTO
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public AddressDTO Address { get; set; }
+    }
+
+    public class AddressDTO
+    {
+        public long Id { get; set; }
+        public string City { get; set; }
+        public string AddressLine { get; set; }
+    }
+
+    public class CreatePersonDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public long? AddressId { get; set; }
+    }
+
+    public class GetAllRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
